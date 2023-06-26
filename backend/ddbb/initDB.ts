f00371fba38d0ec2def673d3b58dab6c4006a965
hns_6 <hector.novoa.segade@gmail.com>
2023-06-26 17:54:39 +0200

    <feat>database: crea el fichero de resetear la bd

--- backend/ddbb/initDB.ts
+++ backend/ddbb/initDB.ts
@@ -13,14 +13,42 @@ async function main(): Promise<void> {
     console.log("Borrando tablas...");
     //BORRADO DE TABLAS
     await connection.query(`DROP TABLE IF EXISTS likes`);
-    await connection.query(`DROP TABLE IF EXISTS comments`);
-    await connection.query(`DROP TABLE IF EXISTS tweets`);
+    await connection.query(`DROP TABLE IF EXISTS entries`);
     await connection.query(`DROP TABLE IF EXISTS users`);
 
     //To do: crear la tabla de users.
+    await connection.query(`CREATE TABLE IF NOT EXISTS users(
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(100) NOT NULL,
+        username VARCHAR(30) NOT NULL,
+        bio VARCHAR(100),
+        avatar VARCHAR(200) DEFAULT "default_avatar.png",
+        registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        activated BOOLEAN DEFAULT FALSE
+        )`);
+
     //To do: crear la tabla de tweets.
-    //To do: crear la tabla de comments.
+    await connection.query(`CREATE TABLE IF NOT EXISTS entries(
+      id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+      user_id int UNSIGNED NOT NULL,
+      parent_entry_id int UNSIGNED,
+      FOREIGN KEY(parent_entry_id) REFERENCES entries_id,
+      FOREIGN KEY(user_id) REFERENCES users(id),
+      text VARCHAR(280) NOT NULL,
+      pictures VARCHAR(100),
+      creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )`);
+
     //To do: crear la tabla de likes.
+    await connection.query(`CREATE TABLE IF NOT EXISTS likes (
+      id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        user_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id),
+        entry_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (entry_id) REFERENCES entries (id),
+        creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
   } catch (error) {
     console.error(error);
   } finally {
@@ -28,4 +56,4 @@ async function main(): Promise<void> {
   }
 }
 
-main;
+main();
