c8b25f2daa36c68547117e2b5f46cc1675b5a42d
hns_6 <hector.novoa.segade@gmail.com>
2023-06-27 12:59:25 +0200

    feat server añade los primeros middlewares del servidor

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import fileUpload from "express-fileupload";
+import morgan from "morgan";
+import cors from "cors";
+
+const { PORT } = process.env;
+
+const app: express.Application = express();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(fileUpload());
