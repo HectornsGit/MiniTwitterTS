b69b6f7803c0ad893a34a96a754eda6b46941d1f
hns_6 <hector.novoa.segade@gmail.com>
2023-06-27 13:31:30 +0200

    feat server añade middlewares de ruta no encontrada y error

--- backend/server.ts
+++ backend/server.ts
@@ -4,9 +4,11 @@ import fileUpload from "express-fileupload";
 import morgan from "morgan";
 import cors from "cors";
 
+dotenv.config();
 const { PORT } = process.env;
-
 const app: express.Application = express();
+
+//------------Middlewares imprescindibles------------//
 
 app.use(cors());
 
@@ -17,3 +19,37 @@ app.use(express.json());
 app.use(fileUpload());
 
 app.use(express.static("uploads"));
+
+//----- Middelwares de error y ruta no encontrada-------//.
+
+interface ErrorResponse {
+  status: number;
+  message: string;
+}
+
+app.use(
+  (
+    err: ErrorResponse,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): void => {
+    console.error(err);
+
+    res.status(err.status || 500).send({
+      status: "error",
+      message: err.message,
+    });
+  }
+);
+
+app.use((req: express.Request, res: express.Response) => {
+  res.status(404).send({
+    status: "error",
+    message: "Ruta no encontrada",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at PORT: ${PORT}`);
+});
