5c1c908dea8f4fc0f07727d84bd4c6ba79c20458
hns_6 <hector.novoa.segade@gmail.com>
2023-07-12 14:20:57 +0200

    fix  backend añade try catch al endpoint de añadir usuario

--- backend/server.ts
+++ backend/server.ts
@@ -24,37 +24,52 @@ import getConnection from "./ddbb/getConnection";
 import mysql = require("mysql2/promise");
 import bcrypt = require("bcrypt");
 
-app.post("/users", async (req: express.Request, res: express.Response) => {
-  const { email, username, password } = req.body;
-  //to do: registrar un usuario.
-  //1: aplicar esquemas de Joi.
-  //2: encriptar la contraseña.
-  const hashedPassword = await bcrypt.hash(password, 10);
-  //3: realizar la petición a la bbdd.
+app.post(
+  "/users",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const { email, username, password } = req.body;
+      //to do: registrar un usuario.
+      //1: aplicar esquemas de Joi.
 
-  let connection: mysql.PoolConnection | void;
-  connection = await getConnection;
+      //2: encriptar la contraseña.
+      const hashedPassword = await bcrypt.hash(password, 10);
+      //3: realizar la petición a la bbdd.
 
-  try {
-    if (typeof connection === "undefined") {
-      throw new Error("Error en la conexión con la base de datos.");
-    }
+      let connection: mysql.PoolConnection | void;
+      connection = await getConnection;
 
-    await connection.query(
-      `INSERT INTO users (email, username, password, registration_date)
-       VALUES (?,?,?,?)`,
-      [email, username, hashedPassword, new Date()]
-    );
-  } catch (err: any) {
-    console.error(err.message);
-  } finally {
-    if (connection) {
-      connection.release();
+      try {
+        if (typeof connection === "undefined") {
+          throw new Error("Error en la conexión con la base de datos.");
+        }
+
+        await connection.query(
+          `INSERT INTO users (email, username, password, registration_date)
+           VALUES (?,?,?,?)`,
+          [email, username, hashedPassword, new Date()]
+        );
+      } catch (err: any) {
+        console.error(err.message);
+      } finally {
+        if (connection) {
+          connection.release();
+        }
+      }
+      //3: devolver la respuesta.
+      res.send({
+        status: "ok",
+        message: "Usuario creado.",
+      });
+    } catch (err) {
+      next(err);
     }
   }
-
-  //3: devolver la respuesta.
-});
+);
 
 //----- Middelwares de error y ruta no encontrada-------//.
 
