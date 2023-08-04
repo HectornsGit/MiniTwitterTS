76583ec45e9729805df7660d465090c4d0445e11
hns_6 <hector.novoa.segade@gmail.com>
2023-08-04 12:01:27 +0200

    refactor selectUserByEmailQuery cambia la aserción de tipos de los resultados y corrige su tipado

---
+++ backend/ddbb/queries/users/selectUserByEmailQuery.ts
@@ -0,0 +1,27 @@
+import { generateError } from "../../../helpers";
+import getConnection from "../../getConnection";
+import mysql = require("mysql2/promise");
+
+export const selectUserByEmailQuery = async (email: string) => {
+  let connection: mysql.PoolConnection | void;
+  connection = await getConnection;
+
+  try {
+    if (typeof connection === "undefined") {
+      throw generateError("Error en la conexión con la base de datos", 500);
+    }
+
+    const users = await connection.query(
+      `SELECT id, password FROM users WHERE email = ?`,
+      [email]
+    );
+
+    if (users.length < 1) {
+      throw generateError("Usuario no encontrado", 404);
+    }
+
+    return users[0];
+  } finally {
+    if (connection) connection.release();
+  }
+};
